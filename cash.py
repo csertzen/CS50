0e4e566b8a8bfa93b2b1c9b0bcd1a98790cc030a
wupasscat <wupasscat10@gmail.com>
2021-03-28 20:38:23 -0400

    Update cash.py
    bugs, bugs, bugs

--- cash.py
+++ cash.py
@@ -1,5 +1,5 @@
 while(True):
-    quarter = 5
+    quarter = 25
     dime = 0
     nickel = 5
     penny = 1
@@ -7,8 +7,11 @@ while(True):
     changeInput = input("Change owed: ")
     usrInput = changeInput.strip("$")
     floatInput = float(usrInput)
-    if floatInput > 0:
+    if floatInput > 0.00:
         coins = floatInput * 100
         cents = 100 - coins
-        
+        while(cents >= 0):
+            cents -= 25
+            print("Cents: ", cents)
+            print("Coins: ", coins)
         
